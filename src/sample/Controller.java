9600c6a4c16dc2d1b7938f61b43955c583743bb9
Alexey Zimin <snowfish@mail.ru>
2019-02-13 13:11:07 +0300

    Almost final version

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class Controller {
+
+    @FXML
+    private Button btn;
+
+    @FXML
+    private void restartGame(ActionEvent e){
+        Main.fillGameField();
+        Main.restoreGameFieldView();
+    }
+
+    public Controller(){
+    }
+
+    @FXML
+    private void initialize(){
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,27 +5,33 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
-import javafx.scene.input.MouseButton;
-import javafx.scene.layout.BackgroundFill;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
 
-    String[] colors = {"GREY","BROWN", "GREEN", "BLUE", "YELLOW", "PURPLE", "MAGENTA", "CYAN", "BLACK", "BLACK","RED"};
+    private static String[] colors = {"GREY","BROWN", "GREEN", "BLUE", "YELLOW", "PURPLE", "MAGENTA", "CYAN", "BLACK", "BLACK","RED"};
+
+    private static String s;
 
     private static Label minesCleared;
     private static Label minesStill;
 
     private static final int fieldSize = 10;
+    private static final int cellSize = 40;
     private static int mineCount = 15;
     private static int[][] gameFieldValues = new int[fieldSize][fieldSize];
+
+    private static Label[][] labels = new Label[fieldSize][fieldSize];     // array of labels-digits on the game field
+    private static Button[][] buttons = new Button[fieldSize][fieldSize];  // array of buttons covering cells and waiting for clicks
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -43,37 +49,19 @@ public class Main extends Application {
         minesStill.setText(""+mineCount);
 
         GridPane gameField = (GridPane) scene.lookup("#GameField");
+        Image image = new Image(getClass().getResourceAsStream("flag.png"));
 
-        Label[][] labels = new Label[fieldSize][fieldSize];     // array of labels-digits on the game field
-        Button[][] buttons = new Button[fieldSize][fieldSize];  // array of buttons covering cells and waiting for clicks
-
-        int fieldSize = 10;
-        String s;
         for (int i = 0; i < fieldSize; i++) {
             for (int j = 0; j < fieldSize; j++) {
 
                 labels[i][j] = new Label("");
-                switch (gameFieldValues[i][j]){
-                    case 0: { s=""; break; }
-                    case 10: { s="*"; labels[i][j].setFont(Font.font("System", FontWeight.BOLD, 40)); break; }
-                    default: s=""+gameFieldValues[i][j];
-                }
-                labels[i][j].setText(s);
-                labels[i][j].setPrefWidth(40);
-                labels[i][j].setPrefHeight(40);
-                labels[i][j].setAlignment(Pos.CENTER);
-                labels[i][j].setTextFill(Paint.valueOf(colors[gameFieldValues[i][j]]));
-                labels[i][j].setFont(Font.font("System", FontWeight.BOLD, 24));
+                defineLabel(i,j);
                 gameField.add(labels[i][j],i,j);
 
                 buttons[i][j] = new Button("");
-                buttons[i][j].setPrefHeight(40);
-                buttons[i][j].setPrefWidth(40);
-                buttons[i][j].setMinHeight(40);
-                buttons[i][j].setMinWidth(40);
-                buttons[i][j].setAlignment(Pos.CENTER);
-                buttons[i][j].setId("X"+i+"Y"+j+"STYLE"+buttons[i][j].getStyle());
+                defineButton(i,j);
 
+//              Добавление обработчика нажатий кнопок мыши для каждой кнопки на игровом поле
                 buttons[i][j].setOnMouseClicked(e -> {
                     Button eventButton = (Button) e.getSource();
                     switch (e.getButton()){
@@ -94,9 +82,8 @@ public class Main extends Application {
                             }
                             else {
                                 eventButton.setText("X");
-                                eventButton.setFont(Font.font("System", FontWeight.BOLD, 24));
-                                eventButton.setTextFill(Paint.valueOf("YELLOW"));
-                                eventButton.setStyle("-fx-background-color: #222222");
+                                eventButton.setGraphic(new ImageView(image));
+                                eventButton.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
                                 mineCount--;
                                 minesStill.setText("" + mineCount);
                             }
@@ -109,8 +96,16 @@ public class Main extends Application {
         }
     }
 
-    private void fillGameField() {
+    static void fillGameField() {
         int mineX, mineY;
+
+        // Обнуление массива значений игрового поля
+        for (int i = 0; i < fieldSize; i++)
+            for (int j = 0; j < fieldSize; j++) {
+                gameFieldValues[i][j]=0;
+            }
+
+        // Расстановка мин по полю
         for (int i = 0; i < mineCount; i++) {
             do {
                 mineX = (int) (Math.random() * fieldSize);
@@ -118,13 +113,52 @@ public class Main extends Application {
             } while (gameFieldValues[mineX][mineY] == 10);
             gameFieldValues[mineX][mineY] = 10;
         }
+
+        // Подсчет значения каждой пустой клетки = количество мин вокруг нее
         for (int i = 0; i < fieldSize; i++)
             for (int j = 0; j < fieldSize; j++)
                 if (gameFieldValues[i][j]<10)
                     for (int k = -1; k < 2; k++)
                         for (int l = -1; l < 2; l++)
-                            if((i+k)>=0  && (i+k)<fieldSize && (j+l)>=0 && (j+l)<fieldSize && (gameFieldValues[i+k][j+l]==10))
+                            if ((i + k) >= 0 && (i + k) < fieldSize && (j + l) >= 0 && (j + l) < fieldSize && (gameFieldValues[i + k][j + l] == 10))
                                 gameFieldValues[i][j]++;
+    }
+
+    static void restoreGameFieldView(){
+        for (int i = 0; i < fieldSize; i++)
+            for (int j = 0; j < fieldSize; j++) {
+                defineLabel(i,j);
+                defineButton(i,j);
+            }
+    }
+
+    private static void defineLabel(int i, int j){
+        String s;
+        switch (gameFieldValues[i][j]){
+            case 0: { s=""; break; }
+            case 10: { s="@"; labels[i][j].setStyle("-fx-background-color: #ffbbbb"); break; }
+            default: s=""+gameFieldValues[i][j];
+        }
+        labels[i][j].setText(s);
+        labels[i][j].setTextFill(Paint.valueOf(colors[gameFieldValues[i][j]]));
+        labels[i][j].setPrefWidth(cellSize);
+        labels[i][j].setMinWidth(cellSize);
+        labels[i][j].setPrefHeight(cellSize);
+        labels[i][j].setMinHeight(cellSize);
+        labels[i][j].setAlignment(Pos.CENTER);
+        labels[i][j].setFont(Font.font("System", FontWeight.BOLD, 24));
+    }
+
+    private static void defineButton(int i, int j){
+        buttons[i][j].setText("");
+        buttons[i][j].setPrefHeight(cellSize);
+        buttons[i][j].setPrefWidth(cellSize);
+        buttons[i][j].setMinHeight(cellSize);
+        buttons[i][j].setMinWidth(cellSize);
+        buttons[i][j].setAlignment(Pos.CENTER);
+        buttons[i][j].setStyle("-fx-base: #aaaaaa");
+        buttons[i][j].setId("X"+i+"Y"+j+"STYLE"+buttons[i][j].getStyle());
+        buttons[i][j].setVisible(true);
     }
 
     public static void main(String[] args) {
